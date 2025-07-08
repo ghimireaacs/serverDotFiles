16698b9fd2cfcfad9c1b321c98cd15a41b1935f8
Prashant Ghimire <aacs.csu@gmail.com>
2025-07-09 03:50:42 +1000

    no binary folder

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 # Get the directory of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-echo "Creating symlinks..."
+echo "Creating symlinks for config files..."
 
 # Create symlinks for config files
 ln -sfv "$DIR/.zshrc" ~/.zshrc
@@ -17,8 +17,6 @@ mkdir -p ~/.oh-my-zsh/custom
 ln -sfv "$DIR/oh-my-zsh-custom/themes" ~/.oh-my-zsh/custom/themes
 ln -sfv "$DIR/oh-my-zsh-custom/plugins" ~/.oh-my-zsh/custom/plugins
 
-# Optional: Make your scripts available in your path
-mkdir -p ~/bin
-ln -sfv "$DIR/scripts/dockstat" ~/bin/dockstat
-
+echo ""
 echo "Done. Please restart your shell or run 'source ~/.zshrc'."
+echo "Your 'dockstat' script is available at: ~/dotfiles/scripts/dockstat"
