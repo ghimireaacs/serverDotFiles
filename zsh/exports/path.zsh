c7d7f146a08efe8f918ad502ed093ce6c2b8933d
ghimireaacs <aacs.csu@gmail.com>
2025-08-07 01:04:47 +0000

    fixed path issue

--- zsh/exports/path.zsh
+++ zsh/exports/path.zsh
@@ -1,1 +1,8 @@
+# 1. Personal scripts and binaries
 export PATH="$HOME/cbin:$PATH"
+
+# 2. User-installed packages (pip, zoxide, etc.)
+export PATH="$HOME/.local/bin:$PATH"
+
+# 3. Go language binaries
+export PATH="$HOME/go/bin:$PATH"
